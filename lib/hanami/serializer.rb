ed02857c219fc16cdbefaca96295811450189ba7
Minoru Mizutani <mmizutani@users.noreply.github.com>
2020-01-06 00:41:33 +0900

    Add a benchmark comparison (#2)
    * Made JSON marshalling engine configurable
    
    * Added a benchmark comparison

--- lib/hanami/serializer.rb
+++ lib/hanami/serializer.rb
@@ -1,9 +1,10 @@
-require "hanami/serializer/base"
-require "hanami/serializer/action"
-require "hanami/serializer/version"
+require 'hanami/serializer/config'
+require 'hanami/serializer/base'
+require 'hanami/serializer/action'
+require 'hanami/serializer/version'
 
 module Hanami
   module Serializer
-    # Your code goes here...
+    extend Configurable
   end
 end
